0e18552b3d733357cca32a698ab1c9ef72e0145e
Adrian Kubaszewski <adrian.kubaszewski@gmail.com>
2016-05-16 18:36:23 +0200

    obsluga VCP

--- Akcelerometr/main.c
+++ Akcelerometr/main.c
@@ -1,80 +1,139 @@
+#include "stm32f4xx_conf.h"
 #include "stm32f4xx.h"
 #include "stm32f4xx_gpio.h"
-#include "stm32f4xx_spi.h"
+#include "usbd_cdc_core.h"
+#include "usbd_usr.h"
+#include "usbd_desc.h"
+#include "usbd_cdc_vcp.h"
+#include "usb_dcd_int.h"
+#include "stm32f4xx_tim.h"
 #include "stm32f4_discovery_lis302dl.h"
 
 int8_t przys_x;
 int8_t przys_y;
 int8_t przys_z;
 
-void Init(){
+char znaki[1]={'X'};
 
-LIS302DL_InitTypeDef LIS302DL_InitStruct;
+__ALIGN_BEGIN USB_OTG_CORE_HANDLE  USB_OTG_dev __ALIGN_END;
 
-// Konfiguracja LIS302DL
-//LIS302DL_InitStruct.Power_Mode=LIS302DL_LOWPOWERMODE_ACTIVE;
-LIS302DL_InitStruct.Output_DataRate=LIS302DL_DATARATE_100;
-LIS302DL_InitStruct.Axes_Enable=LIS302DL_X_ENABLE | LIS302DL_Y_ENABLE | LIS302DL_Z_ENABLE;
-LIS302DL_InitStruct.Full_Scale=LIS302DL_FULLSCALE_2_3;
-LIS302DL_InitStruct.Self_Test=LIS302DL_SELFTEST_NORMAL;
-LIS302DL_Init(&LIS302DL_InitStruct);
+#define HSE_VALUE    ((uint32_t)8000000)
 
-SystemInit();
-GPIO_InitTypeDef  GPIO_InitStructure;
-RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+ void init()
+ {
+	 LIS302DL_InitTypeDef AccInitStr;
+	 	AccInitStr.Axes_Enable = LIS302DL_XYZ_ENABLE;
+	 	AccInitStr.Full_Scale = LIS302DL_FULLSCALE_2_3;
+	 	AccInitStr.Power_Mode = LIS302DL_LOWPOWERMODE_ACTIVE;
+	 	AccInitStr.Output_DataRate = LIS302DL_DATARATE_100;
+	 	AccInitStr.Self_Test = LIS302DL_SELFTEST_NORMAL;
+	 	LIS302DL_Init(&AccInitStr);
 
-GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-GPIO_Init(GPIOD, &GPIO_InitStructure);
+ 	GPIO_InitTypeDef  GPIO_InitStructure;
+ 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-// OpóŸnienie
-for (int i=0;i<100000;i++);
+ 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+ 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+ 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+ 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+ 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+ 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-}
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+ 	USBD_Init(&USB_OTG_dev,USB_OTG_FS_CORE_ID,&USR_desc,&USBD_CDC_cb,&USR_cb);
+ }
+
+
+ void OTG_FS_IRQHandler(void)
+ {
+   USBD_OTG_ISR_Handler (&USB_OTG_dev);
+ }
+
+ void OTG_FS_WKUP_IRQHandler(void)
+ {
+   if(USB_OTG_dev.cfg.low_power)
+   {
+     *(uint32_t *)(0xE000ED10) &= 0xFFFFFFF9 ;
+     SystemInit();
+     USB_OTG_UngateClock(&USB_OTG_dev);
+   }
+ }
+
 
 int main(void)
 {
+	SystemInit();
 
-	Init();
+	for(int i =0; i<50000;i++);
 
-    while(1)
-    {
+	init();
 
-    	LIS302DL_Read(&przys_x, LIS302DL_OUT_X_ADDR, 1);
-    	LIS302DL_Read(&przys_y, LIS302DL_OUT_Y_ADDR, 1);
+	unsigned int i;
+	int timer;
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_TimeBaseStructure.TIM_Period = 20000;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseStructure.TIM_Prescaler = 500;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+	TIM_Cmd(TIM2, ENABLE);
 
-    	//LIS302DL_Read(&przys_z, LIS302DL_OUT_Z_ADDR, 1);
+	
 
-    	for(int i=0; i<100000; i++){};
+	while (1){
 
+		LIS302DL_Read(&przys_x, LIS302DL_OUT_X_ADDR, 1);
+		LIS302DL_Read(&przys_y, LIS302DL_OUT_Y_ADDR, 1);
+		timer = TIM_GetCounter(TIM2);
 
+		if(przys_x>30)
+		{
+			znaki[0]='A';
+			GPIO_SetBits(GPIOD,GPIO_Pin_14);
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_15|GPIO_Pin_13);
+		}
 
-    	if(przys_x>0)
-    			{
-    				GPIO_SetBits(GPIOD,GPIO_Pin_14);
-    				GPIO_ResetBits(GPIOD, GPIO_Pin_12);
-    			}
+		if(przys_y<-30)
+		{
+			znaki[0]='W';
+			GPIO_SetBits(GPIOD,GPIO_Pin_15);
+			GPIO_ResetBits(GPIOD, GPIO_Pin_13|GPIO_Pin_12|GPIO_Pin_14);
+		}
 
-    	if(przys_y>0)
-    	    			{
-    	    				GPIO_SetBits(GPIOD,GPIO_Pin_13);
-    	    				GPIO_ResetBits(GPIOD, GPIO_Pin_15);
-    	    			}
+		if(przys_x<-30)
+		{
+			znaki[0]='D';
+			GPIO_SetBits(GPIOD,GPIO_Pin_12);
+			GPIO_ResetBits(GPIOD, GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+		}
 
-    	if(przys_x<0)
-    			{
-    				GPIO_SetBits(GPIOD,GPIO_Pin_12);
-    				GPIO_ResetBits(GPIOD, GPIO_Pin_14);
-    			}
+		if(przys_y>30)
+		{
+				znaki[0]='S';
+				GPIO_SetBits(GPIOD,GPIO_Pin_13);
+				GPIO_ResetBits(GPIOD, GPIO_Pin_14|GPIO_Pin_15|GPIO_Pin_12);
+		}
 
-    	if(przys_y<0)
-    	    			{
-    	    				GPIO_SetBits(GPIOD,GPIO_Pin_15);
-    	    				GPIO_ResetBits(GPIOD, GPIO_Pin_13);
-    	    			}
+		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0))
+		{
+			znaki[0]='B';
+			GPIO_SetBits(GPIOD, GPIO_Pin_14|GPIO_Pin_15|GPIO_Pin_12|GPIO_Pin_13);
+		}
 
-    }
+		if(timerValue==500){
+			VCP_send_buffer(&znaki,1);
+		}
+		GPIO_ResetBits(GPIOD, GPIO_Pin_14|GPIO_Pin_15|GPIO_Pin_12|GPIO_Pin_13);
+	}
+	return 0;
 }
+
+
